12fe741d5f31b6b7a416bdcb263f90a347f94e5f
irenegp06 <irenegp06@gmail.com>
2026-09-22 19:24:34 +0200

    main.cc

--- main.cc
+++ main.cc
@@ -44,7 +44,54 @@ int main() {
                 cout << "INVALIDCM" << "\n";
                 continue;
             }
-            string title
+            string title = replaceUnderscores(titleRaw);
+            Resource r(id, year, title);
+            if(tagsStr != "-"){
+                istringstream tss(tagsStr);
+                string tag;
+                while (getline(tss, tag, ',')){
+                    r.addTag(tag);
+                }
+            }
+            if (catalog.add(r)){
+                cout << "OK" << "\n"
+            }else{
+                cout << "DUPLICATE" << "\n";
+            }
+        }
+        else if ( command == "DEL"){
+            string id;
+            if(!(iss>>id)){
+                cout << "INVALIDCMD" << "\n";
+                continue;
+            }
+            if (catalog.removeById(id)){
+                 cout << "OK" << "\n"
+            }else{
+                cout << "NOTFOUND" << "\n";
+            }
+            
+        }else if (command == "GET"){
+            string id;
+            if(!(iss>>id)){
+                cout << "INVALIDCM" << "\n";
+                continue;
+            }
+            Resource r = catalog.findById(id);
+            if (r.getId().empty()){
+                cout << "NOTFOUND" << "\n";
+               
+            }else{
+                cout << r << "\n";
+                continue;
+            }
+            vector<Resource> results = catalog.findByIdTag(tag);
+            cout << "COUNT" << results.size() << "\n";
+            for (size_t i = 0; i < result.size(); ++i){
+                cout << results [i] << "\n";
+            }   
+        }else{
+            cout << "INVALIDCM" << "\n";
         }
 
         // TODO: procesa el resto de comandos (LOAD, ADD, DEL, GET, FINDTAG)
